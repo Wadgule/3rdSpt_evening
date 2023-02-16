23bdaf44c401c7c0d4fea278defe852e3c8fe850
rajendrawadgule <rajendrawadgule@gmail.com>
2023-02-16 12:39:33 +0530

    [Rajendra]:-  demmo  class is wrong

---
+++ src/Raj3/Rajesh.java
@@ -0,0 +1,10 @@
+package Raj3;
+
+public class Rajesh {
+
+	public static void main(String [] args) 
+	{
+	System.out.println("Rajendra wadgule");
+	}
+	
+}
---
+++ src/sample/demmo.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class demmo
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Rajendra");
+		System.out.println("Hello");
+		
+	}
+
+}
